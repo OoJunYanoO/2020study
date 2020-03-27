312301a785aacf1835d8cece563311ca0e7a5f6f
junyan <719364082@qq.com>
2020-03-27 15:57:11 +0800

    hellowworld

---
+++ test/01javaSE/01helloworld/src/HellowWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: JunYan
+ * @Description:
+ * @Date: Created in  2020/3/27
+ * @company: com.junyan
+ */
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
